e1138ee87b1384958599a356ff224136146af89c
Michael Fuchs <admin@dekilla.at>
2017-01-11 15:07:00 +0100

    adding spring

---
+++ src/main/java/at/fh/ima/swengs/petcard/PetCardApplication.java
@@ -0,0 +1,12 @@
+package at.fh.ima.swengs.petcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetCardApplication.class, args);
+	}
+}
