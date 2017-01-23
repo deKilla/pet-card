25774c28ff244752b9adef2715643af7d5e15aa4
Michael Fuchs <admin@dekilla.at>
2017-01-23 13:34:16 +0100

    footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,19 @@
+$(window).load(function(){
 
+  alert($("body").height());
+  alert($(window).height());
+
+  if ($("body").height() > $(window).height()) {
+    $("footer").hide();
+    alert("footer should be hidden");
+    window.onscroll = function (ev) {
+      if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        $("footer").slideDown();
+        alert("footer should be visible again");
+      } else {
+        $("footer").slideUp();
+        alert("footer should be hidden again");
+      }
+    };
+  };
+});
