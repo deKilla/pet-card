9311042ebbd3ac427b4be599e93a875ea70fabb1
Michael Fuchs <admin@dekilla.at>
2017-01-25 18:10:14 +0100

    doctor home done

--- src/app/home/doctor-home.component.ts
+++ src/app/home/doctor-home.component.ts
@@ -1,6 +1,7 @@
 import {Pet} from "../entities/pet";
 import { Component } from '@angular/core';
 import {PetService} from "../services/pet.service";
+import {OAuthService} from "angular-oauth2-oidc";
 
 @Component({
   templateUrl: "./doctor-home.component.html"
@@ -10,7 +11,7 @@ export class DoctorHomeComponent {
 
   public id: string;
 
-  constructor(private petService:PetService) {
+  constructor(private petService:PetService, private oauthService: OAuthService) {
   }
 
 
@@ -20,6 +21,13 @@ export class DoctorHomeComponent {
 
   search (): void{
     this.petService.findById(this.id);
+  }
+
+  get givenName(): string {
+    let claims = this.oauthService.getIdentityClaims();
+    if (!claims) return null;
+
+    return claims.given_name;
   }
 }
 
