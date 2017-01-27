957a843fc9dad476d4850aa208f121f216937c5f
Nina Spalek <nina.spalek@gmx.at>
2017-01-27 19:22:46 +0100

    solved 500 server error; show medication in pet info ok

--- src/main/java/at/fh/ima/swengs/petcard/model/PetMedicationRepository.java
+++ src/main/java/at/fh/ima/swengs/petcard/model/PetMedicationRepository.java
@@ -4,12 +4,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 
 public interface PetMedicationRepository extends PagingAndSortingRepository<PetMedication, Long> {
 
   public PetMedication findById(@Param("id") long id);
 
   @Query(value = "select pm.* from pet_medication pm join pet p on p.id = pm.pet_id where p.id = ?1", nativeQuery = true)
-  public PetOwner findByPet(@Param("id") long id);
+  public List<PetMedication> findByPet(@Param("id") long id);
 
 }
