b952019046995c35913bf8f587f0faad67ad6397
Michael Fuchs <admin@dekilla.at>
2017-01-31 16:46:46 +0100

    changed disease/medication order to match information from petDisease and petMedication

--- src/main/java/at/fh/ima/swengs/petcard/model/DiseaseRepository.java
+++ src/main/java/at/fh/ima/swengs/petcard/model/DiseaseRepository.java
@@ -12,7 +12,7 @@ public interface DiseaseRepository extends PagingAndSortingRepository<Disease, L
   public Disease findById(@Param("id") long id);
 
   //selects all diseases by one pet
-  @Query(value = "select d.* from disease d join pet_disease pd on d.id = pd.disease_id join pet p on pd.pet_id = p.id where p.id = ?1", nativeQuery = true)
+  @Query(value = "select d.* from disease d join pet_disease pd on d.id = pd.disease_id join pet p on pd.pet_id = p.id where p.id = ?1 order by pd.id", nativeQuery = true)
   public List<Disease> findByPet(@Param("id") long id);
 
 }
