1f37cf236b51459a1e43b7dffcf653573964690c
Nina Spalek <nina.spalek@gmx.at>
2017-02-01 13:24:10 +0100

    simplified edit method

--- src/app/edit/edit-pet.component.ts
+++ src/app/edit/edit-pet.component.ts
@@ -5,8 +5,7 @@ import {PetOwner} from "../entities/petOwner";
 import {Doctor} from "../entities/doctor";
 import {DoctorService} from "../services/doctor.service";
 import {PetOwnerService} from "../services/petOwner.service";
-import {Router, ActivatedRoute} from "@angular/router";
-import {PetInfoComponent} from "../pet-info/pet-info.component";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'edit-pet',
@@ -15,29 +14,14 @@ import {PetInfoComponent} from "../pet-info/pet-info.component";
 export class EditPetComponent {
 
   //fields from html
-  public name: string;
-  public weight: number;
   public ownerId: string;
   public doctorId: string;
-
-  private petId: number;
-  private type: string;
-  private birthdate: string;
-  private testPet: Pet;
+  public pet: Pet;
 
   constructor(private petOwnerService:PetOwnerService, private petService:PetService, private doctorService:DoctorService,
               private router: Router) {
 
-    this.testPet = this.petService.pet;
-
-    this.name = this.petService.pet.name;
-    this.weight = this.petService.pet.weight;
-    this.petId = this.petService.pet.id;
-    this.type = this.petService.pet.type;
-    this.birthdate = this.petService.pet.birthDate;
-
-    this.petOwnerService.findByPet(this.petId.toString());
-    this.doctorService.findByPet(this.petId.toString());
+    this.pet = this.petService.pet;
     this.ownerId = this.petOwnerService.petOwner.id.toString();
     this.doctorId = this.doctorService.doctor.id.toString();
 
@@ -56,16 +40,10 @@ export class EditPetComponent {
 
   //saves changes from pet
   save(): void{
-    this.testPet.name = this.name;
-    this.testPet.weight = this.weight;
-    this.testPet.id = this.petId;
-    this.testPet.birthDate = this.birthdate;
-    this.testPet.type = this.type;
-
     //gets promise, so that the new entry is added to the db befor redirecting and reloading the pet info
-    let promise = this.petService.save(this.testPet, this.doctorId, this.ownerId).toPromise();
+    let promise = this.petService.save(this.pet, this.doctorId, this.ownerId).toPromise();
     promise.then(() => {
-      this.router.navigate(['petInfo', {id:this.petId}]);
+      this.router.navigate(['petInfo', {id:this.pet.id}]);
     });
   }
 }
